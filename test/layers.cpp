cd89b714e9f67a568765a30ce9b671855536fdb4
Tobias Pohlen <tobias.pohlen@gmail.com>
2016-12-12 23:07:06 +0100

    oops

--- test/layers.cpp
+++ test/layers.cpp
@@ -335,3 +335,65 @@ TEST(Conv2DLayer, W_initializer_2)
     ASSERT_EQ(1, outputShape[4]);
     ASSERT_FLOAT_EQ(9, output(0, 0, 0, 0, 0));
 }
+
+TEST(Conv2DLayer, W_eigen)
+{
+    // Arrange
+    auto device = CNTK::DeviceDescriptor::CPUDevice();
+    auto X = CNTK::InputVariable({ 3, 3, 1 }, CNTK::DataType::Float);
+    CNTK::FunctionPtr network;
+
+    // Create a filter in Eigen
+    Eigen::Tensor<float, 4> W(3, 3, 1, 1);
+    W(0, 0, 0, 0) = 1.0f;
+    W(0, 1, 0, 0) = 1.0f / 2.0f;
+    W(0, 2, 0, 0) = 1.0f / 3.0f;
+    W(1, 0, 0, 0) = 1.0f / 4.0f;
+    W(1, 1, 0, 0) = 1.0f / 5.0f;
+    W(1, 2, 0, 0) = 1.0f / 6.0f;
+    W(2, 0, 0, 0) = 1.0f / 7.0f;
+    W(2, 1, 0, 0) = 1.0f / 8.0f;
+    W(2, 2, 0, 0) = 1.0f / 9.0f;
+
+
+    // Act
+    network = Chianti::Layers::Conv2DLayer(X, device)
+            .filterSize({3, 3})
+            .pad("valid")
+            .stride(1)
+            .numFilters(1)
+            .W(W);
+
+    auto outputVar = network->Output();
+
+    auto inputShape = X.Shape().AppendShape({1, 1});
+    auto outputShape = outputVar.Shape().AppendShape({1, 1});
+
+    Eigen::Tensor<float, 5> input(Chianti::Util::convertShape<5>(inputShape));
+    Eigen::Tensor<float, 5> output(Chianti::Util::convertShape<5>(outputShape));
+
+    input(0, 0, 0, 0, 0) = 1.0f;
+    input(0, 1, 0, 0, 0) = 2.0f;
+    input(0, 2, 0, 0, 0) = 3.0f;
+    input(1, 0, 0, 0, 0) = 4.0f;
+    input(1, 1, 0, 0, 0) = 5.0f;
+    input(1, 2, 0, 0, 0) = 6.0f;
+    input(2, 0, 0, 0, 0) = 7.0f;
+    input(2, 1, 0, 0, 0) = 8.0f;
+    input(2, 2, 0, 0, 0) = 9.0f;
+
+    auto inputValue = Chianti::Util::tensorToValue(input);
+    auto outputValue = Chianti::Util::tensorToValue(output);
+
+    std::unordered_map<CNTK::Variable, CNTK::ValuePtr> outputs = {{outputVar, outputValue}};
+
+    network->Forward({{X, inputValue}}, outputs, device);
+
+    // Assert
+    ASSERT_EQ(1, outputShape[0]);
+    ASSERT_EQ(1, outputShape[1]);
+    ASSERT_EQ(1, outputShape[2]);
+    ASSERT_EQ(1, outputShape[3]);
+    ASSERT_EQ(1, outputShape[4]);
+    ASSERT_FLOAT_EQ(9.0f, output(0, 0, 0, 0, 0));
+}
